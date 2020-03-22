94431cf5dd6b7e8e6e4a5336a3d15f4c481a3a30
Mateuskwz <mateus.takeshi@ykt.com.br>
2020-03-22 16:31:46 -0300

    Alterações do código

--- Atividade1-arquitetura/src/main/java/com/mycompany/atividade1/arquitetura/Principal.java
+++ Atividade1-arquitetura/src/main/java/com/mycompany/atividade1/arquitetura/Principal.java
@@ -3,60 +3,59 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-        
-
 package com.mycompany.atividade1.arquitetura;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.Scanner;
-import java.util.List;
-
+import java.util.Set;
 /**
  *
  * @author Aluno
  */
 public class Principal {
+
     public static void main(String[] args) {
-        int opcao = 0, cont = 0, verif = 0;
-        String aux;
-//        String[] alunos = ;
-        ArrayList<String> alunos = new ArrayList();
+        int opcao = 0;
+        Set<String> alunos = new HashSet();
         Scanner read = new Scanner(System.in);
-        while(opcao != 3) {
+
+        while (opcao != 3) {
             System.out.println("Bem vindo ao sistema de ...");
             System.out.println("Escolha uma opção: ");
             System.out.println("[1] Cadastrar aluno");
             System.out.println("[2] Listar alunos cadastrados");
             System.out.println("[3] Sair");
+            System.out.print("Opção: ");
 
             opcao = Integer.parseInt(read.nextLine());
 
-            switch(opcao) {
+            switch (opcao) {
                 case 1:
-                    System.out.println("Digite o nome do aluno: ");
-                    aux = read.nextLine();
-                    Iterator itr1 = alunos.iterator();
-                    while(itr1.hasNext()) {
-                        Object elemento = itr1.next();
-                        if (elemento.equals(aux)) {
-                            System.out.println("Aluno já cadastrado");
-                            verif++;
-                        }
-                    }
-                    if (verif == 0) {
-                        alunos.add(aux);   
-                    }
+                    inserirAluno(read, alunos);
                     break;
                 case 2:
-                    Iterator itr2 = alunos.iterator();
-                    while(itr2.hasNext()) {
-                        Object elemento = itr2.next();
-                        System.out.println(elemento);
-                    }
+                    imprimeLista(alunos);
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("\nOpção inválida!\n");
                     break;
             }
         }
+    }
+
+    private static void inserirAluno(Scanner r, Set<String> listaAlunos) {
+        System.out.print("\nInsira o nome do aluno: ");
+        listaAlunos.add(r.nextLine());
+        System.out.println("\n");
+    }
+    
+    private static void imprimeLista(Set<String> listaAlunos) {
+        System.out.println("\nLista de alunos:\n");
+        for(String alunos : listaAlunos) {
+            System.out.println(alunos);
+        }
+        System.out.println("\n");
     }
 }
